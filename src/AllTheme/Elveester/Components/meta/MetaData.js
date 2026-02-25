712b33bfa58f8f082369a449bb4a3f9b480cc49e
Rajan <optigoapi@gmail.com>
2026-02-25 11:31:18 +0530

    changes in metadata

--- src/AllTheme/Elveester/Components/meta/MetaData.js
+++ src/AllTheme/Elveester/Components/meta/MetaData.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { IsSetupFor } from '../Recoil/atom';
+
+const siteName = IsSetupFor ? "Sonasons" : "Elvee";
 
 const MetaData = ({ title, description, canonical, keywords }) => {
     return (
@@ -10,7 +13,9 @@ const MetaData = ({ title, description, canonical, keywords }) => {
             {/* Meta Tags */}
             {description && <meta name="description" content={description} />}
             {canonical && <link rel="canonical" href={canonical} />}
+            {canonical && <meta property="og:url" content={canonical} />}
             {title && <meta property="og:title" content={title} />}
+            <meta property="og:site_name" content={siteName} />
             {keywords && <meta name="keywords" content={keywords} />}
             {description && <meta property="og:description" content={description} />}
             <meta property="og:type" content="website" />
